6411882fe6871e6b324596b2f0d366aafaa2f317
ibunubi <m.aliimron3@gmail.com>
2017-01-29 17:54:18 +0700

    add Content

---
+++ app/components/Content.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: [], content:[]};
+
+  }
+  componentDidMount(){
+    fetch('../../data/table-of-content.json')
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({items: data});
+    });
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.props.params.contentId != nextProps.params.contentId){
+
+      if(typeof this.state.items[nextProps.params.contentId] != 'undefined'){
+        let onDisk = this.state.items[nextProps.params.contentId].onDisk;
+        onDisk = onDisk.replace('./', '../../');
+        fetch(onDisk)
+        .then((res) => res.json())
+        .then((data) => {
+          this.setState({content: data});
+
+          console.log(this.state.content);
+        });
+      }
+
+    }
+    return (this.props.params.contentId != nextProps.params.contentId);
+  }
+  render() {
+    let content = <div>
+      click table of content on the side bar
+    </div>
+    if(this.state.content.hasOwnProperty('content')){
+      let mainContent = this.state.content['content'];
+      if(mainContent != null) {
+        mainContent = mainContent.replace(/.\/data/g, '../../data');
+      } else {
+        mainContent = `<a href='${this.state.content['url']}'>This content is downloaded yet</a>`;
+      }
+      content = <div>
+        <h1 dangerouslySetInnerHTML={{__html: `${this.state.content['title']}` }} />
+        <div dangerouslySetInnerHTML={{__html: `${mainContent}` }} />
+      </div>;
+    }
+    return (content);
+  }
+}
+
+export default Content;

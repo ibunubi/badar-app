61a5efd91e2725ec7c47757ade69ada1818c26a9
ibunubi <m.aliimron3@gmail.com>
2017-01-30 22:54:16 +0700

    fix audio

--- app/components/Content.js
+++ app/components/Content.js
@@ -41,8 +41,16 @@ class Content extends Component {
       } else {
         mainContent = `<a href='${this.state.content['url']}'>Content for this sub menu is not downloaded properly !</a>`;
       }
+      let audioFile = this.state.content['soundFile'];
+      if(audioFile != '-'){
+        audioFile = <audio ref="audio_tag" src={audioFile} controls autoPlay="true"/>;
+      } else {
+        audioFile = <em>audio not available.</em>;
+      }
+      audioFile = <div className="audio">{audioFile}</div>;
       content = <div className="paper">
         <h1 dangerouslySetInnerHTML={{__html: `${this.state.content['title']}` }} />
+        {audioFile}
         <div dangerouslySetInnerHTML={{__html: `${mainContent}` }} />
       </div>;
     }
